f65e5d332b9cc4b05bb453f4feb13b484541cf71
AbinZorto <47540642+AbinZorto@users.noreply.github.com>
2025-02-18 15:08:44 +0000

    Update server.js

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ app.post('/compile', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`LaTeX service running on port ${PORT}`));
